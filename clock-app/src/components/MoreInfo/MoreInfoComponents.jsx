5722b88eb6c92e2a249bc7c68a0cfe0c1f5d9110
Sreeharsh <sreeharshrajan@gmail.com>
2023-03-28 02:30:20 +0530

    Updates

--- clock-app/src/components/MoreInfo/MoreInfoComponents.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const MoreInfoContainer = styled.div`
-  height: 50vh;
-  background: linear-gradient(#979797, #fff);
-  opacity: 0.95;
-  transition: 0.5s all ease-in-out;
-`
